315fc518d9700a7d67b0b7aa151ac033a209671f
Max Dayton <daytonmaximus@gmail.com>
2020-04-04 14:46:41 -0600

    Added Products, Product containers

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/NavBar/Navbar';
 import Titlecard from './components/Titlecard/Titlecard';
+import Products from './containers/Products/Products';
 
 function App() {
 	return (
 		<div>
 			<Navbar />
 			<Titlecard />
+			<Products />
 		</div>
 	)
 }
---
+++ src/containers/Products/Products.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Product from './Product/Product';
+
+class Products extends Component {
+    render() {
+        return (
+            <div>
+                <Product />
+            </div>
+        )
+    }
+};
+
+export default Products;

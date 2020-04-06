e497b2adb4cebbc5ee9be72e46eed1a58ff26666
Max Dayton <daytonmaximus@gmail.com>
2020-04-06 15:42:17 -0600

    product component styling

--- src/containers/Products/Product/Product.js
+++ src/containers/Products/Product/Product.js
@@ -9,12 +9,14 @@ class Product extends Component {
             <div className={classes.Product}>
                 <img src={sucklingPig} alt="Suckling Pig" />
                 <div className={classes.Info}>
-                    <p>Suckling Pig</p>
-                    <p>$120 - $200</p>
-                    <hr />
+                    <h4>Suckling Pig</h4>
+                    <h3>$120.00 - $200.00</h3>
+                    <hr align="left" />
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id neque aliquam vestibulum morbi blandit cursus. Ornare arcu dui vivamus arcu. Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Lectus quam id leo in vitae turpis. Quisque sagittis purus sit amet volutpat. Amet est placerat in egestas erat imperdiet sed euismod nisi. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
                     <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</strong></p>
+                    <form className={classes.ProductForm}>
+                        
+                    </form>
                 </div>                
             </div>
         )

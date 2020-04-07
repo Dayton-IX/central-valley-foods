de53058ce3e2e1abde18f915ea2281b6827e1827
Max Dayton <daytonmaximus@gmail.com>
2020-04-07 16:02:46 -0600

    added productInfo object to inject dynamic values into product container

--- src/containers/Products/Product/Product.js
+++ src/containers/Products/Product/Product.js
@@ -4,16 +4,32 @@ import sucklingPig from '../../../assets/img/suckling-pig.jpeg';
 import classes from './Product.module.css';
 
 class Product extends Component {
+    productInfo = {
+        name: 'Suckling Pig',
+        image: sucklingPig,
+        text: {
+            normal: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id neque aliquam vestibulum morbi blandit cursus. Ornare arcu dui vivamus arcu. Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Lectus quam id leo in vitae turpis. Quisque sagittis purus sit amet volutpat. Amet est placerat in egestas erat imperdiet sed euismod nisi. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat.',
+            bold: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        },
+        sizes: [
+            {id: 1, value: '10 - 15 lbs', price: 95.00},
+            {id: 2, value: '16 - 21 lbs', price: 110.00},
+            {id: 3, value: '22 - 27 lbs', price: 115.00},
+            {id: 4, value: '28 - 33 lbs', price: 125.00},
+        ],
+        
+    }
+
     render() {
         return (
             <div className={classes.Product}>
-                <img src={sucklingPig} alt="Suckling Pig" />
+                <img src={this.productInfo.image} alt={this.productInfo.name} />
                 <div className={classes.Info}>
-                    <h4>Suckling Pig</h4>
-                    <h3>$120.00 - $200.00</h3>
+                    <h4>{this.productInfo.name}</h4>
+                    <h3>${parseFloat(this.productInfo.sizes[0].price).toFixed(2)} - ${parseFloat(this.productInfo.sizes[this.productInfo.sizes.length - 1].price).toFixed(2)}</h3>
                     <hr align="left" />
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id neque aliquam vestibulum morbi blandit cursus. Ornare arcu dui vivamus arcu. Vitae aliquet nec ullamcorper sit amet risus nullam eget felis. Fames ac turpis egestas maecenas pharetra convallis posuere morbi. Lectus quam id leo in vitae turpis. Quisque sagittis purus sit amet volutpat. Amet est placerat in egestas erat imperdiet sed euismod nisi. Risus commodo viverra maecenas accumsan lacus vel facilisis volutpat.</p>
-                    <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</strong></p>
+                    <p>{this.productInfo.text.normal}</p>
+                    <p><strong>{this.productInfo.text.bold}</strong></p>
                     <form className={classes.ProductForm}>
                         <label for="size">Size:</label>
                         <select id="size">

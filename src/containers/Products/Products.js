08773a5a7fc65b8f52e3b582e403b4d65a6719ef
Max Dayton <daytonmaximus@gmail.com>
2020-04-28 20:11:08 -0600

    typo fix, horizontal scroll fix

--- src/containers/Products/Products.js
+++ src/containers/Products/Products.js
@@ -9,7 +9,7 @@ class Products extends Component {
             <div className={classes.Products}>
                 <h3 className={classes.ProductsHeader}>Products</h3>
                 <hr />
-                <p className={classes.ProductsDescription}>Falslev Livestock and it's new branch, Central Valley Foods, hav been raising pigs for over 50 years. We have been supplying roaster and suckling pigs across the United States. Our goal is to provide fresh pork straight from the farm to the family table. <br />Our suckling and roasting pigs are raised naturally without antibiotics or hormones. The smaller pigs, 10-25 lbs, are on a strict milk diet and the 25-45 lbs are on a healthy diet. The suckling pig is very tender and has great flavor.</p>
+                <p className={classes.ProductsDescription}>Falslev Livestock and it's new branch, Central Valley Foods, have been raising pigs for over 50 years. We have been supplying roaster and suckling pigs across the United States. Our goal is to provide fresh pork straight from the farm to the family table. <br />Our suckling and roasting pigs are raised naturally without antibiotics or hormones. The smaller pigs, 10-25 lbs, are on a strict milk diet and the 25-45 lbs are on a healthy diet. The suckling pig is very tender and has great flavor.</p>
                 <Product type="suckling"/>
                 <Product type="roaster" />
                 <Product type="sausage" />
